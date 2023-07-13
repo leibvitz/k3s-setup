3bfd3171cb544667a59a066c2c440602fa9d9d41
leibvitz <leibvitzssariews@gmail.com>
2023-07-13 19:31:28 +0200

    Customize CA root

--- cluster-system/cert-manager/install.sh
+++ cluster-system/cert-manager/install.sh
@@ -4,7 +4,7 @@ helm repo add jetstack https://charts.jetstack.io --force-update
 if [ ! -f certs/tls.crt ] || [ ! -f certs/tls.key ]; then
   echo "Generating new self-signed root certificate"
   openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 \
-    -subj "/C=DE/ST=Berlin/L=Berlin/CN=LocalDeveloperRootCA" \
+    -subj "/C=ES/ST=Tarragona/L=Vila-seca/CN=LocalDeveloperRootCA" \
     -keyout certs/tls.key -out certs/tls.crt
 fi
 kubectl apply -k certs -n cluster-system
